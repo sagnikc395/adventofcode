773a5c841b2bae597fe8cecbd3cc2a9bf20f8872
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 23:42:39 +0530

    src: solved part1 and part2

---
+++ aoc2020/day5/part2.ts
@@ -0,0 +1,47 @@
+import * as fs from "node:fs/promises";
+
+const calculateRange = (min: number, max: number): number => {
+  return max - Math.round((max - min) / 2);
+};
+
+const calculateLetters = (seatNumbers: string[], numberOfSeats: 127 | 7) => {
+  let max = +numberOfSeats + 1;
+  let min = 0;
+  seatNumbers.forEach((letter) => {
+    switch (letter) {
+      case "F":
+      case "L":
+        max = calculateRange(min, max);
+        break;
+      case "B":
+      case "R":
+        min = calculateRange(min, max);
+        break;
+      default:
+        throw Error("seatLetter not supported");
+    }
+  });
+  return min;
+};
+
+const calculateSeating = (seatNumber: string) => {
+  const rowSeatingNumbers = seatNumber.split("").slice(0, 7);
+  const columnSeatingNumbers = seatNumber.split("").slice(7, 10);
+  const row = calculateLetters(rowSeatingNumbers, 127);
+  const column = calculateLetters(columnSeatingNumbers, 7);
+  const seatId = +row * 8 + +column;
+  return seatId;
+};
+
+async function main() {
+  const data = (await fs.readFile("./input.txt", "utf-8")).split("\n");
+
+  const maxSeatId = data
+    .map((seatNumber) => calculateSeating(seatNumber))
+    .sort((a, b) => a - b)
+    .find((id, index, array) => array[index + 1] !== id + 1);
+
+  console.log(Number(maxSeatId) + 1);
+}
+
+await main();
