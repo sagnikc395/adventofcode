2634934f7050852d49cf8d55f2062e208b896a9b
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-12-10 22:49:09 +0530

    src: solved part2

--- aoc2020/day2/part2.ts
+++ aoc2020/day2/part2.ts
@@ -1,24 +1,25 @@
 import * as fs from "node:fs/promises";
 
 async function main() {
-  const data = (await fs.readFile("./input.txt", "utf-8")).trim().split("\n");
-  let countValidPass = 0;
-  for (const line of data) {
-    let [policy, passw] = line.split(":");
-    let [count, char] = policy.split(" ");
-    let [lowC, upperC] = count.split("-");
+  const data = (await fs.readFile("./input.txt", "utf-8"))
+    .trim()
+    .split("\n")
+    .reduce((occurences, row) => {
+      const password = row.split(" ")[2];
+      const digit1 = +row.split("-")[0];
+      const digit2 = +row.split("-")[1].split(" ")[0];
+      const letter = row.split(" ")[1][0];
 
-    let charCount = 0;
-    for (const c of passw) {
-      if (c == char) {
-        charCount += 1;
-      }
-    }
-    if (charCount >= Number(lowC) && charCount <= Number(upperC)) {
-      countValidPass += 1;
-    }
-  }
-  console.log(countValidPass);
+      const isPositionForLetterOneOccuring = password[digit1 - 1] === letter;
+      const isPositionForLetterTwoOccuring = password[digit2 - 1] === letter;
+
+      //xor
+      const isOnlyOneOccuring =
+        isPositionForLetterOneOccuring !== isPositionForLetterTwoOccuring;
+
+      return isOnlyOneOccuring ? (+occurences + 1).toString() : occurences;
+    }, "0");
+    console.log(data);
 }
 
 await main();
