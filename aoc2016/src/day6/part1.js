07864f397f0d4442892f3b259fdc1938ac57ccba
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-01-19 23:31:53 +0530

    src: solved part1

---
+++ aoc2016/src/day6/part1.js
@@ -0,0 +1,42 @@
+import * as fs from "fs";
+
+function mostFrequentElement(str) {
+  let map = new Map();
+  for (let i = 0; i < str.length; i++) {
+    if (map.has(str[i])) {
+      let temp = map.get(str[i]);
+      map.set(str[i], temp + 1);
+    } else {
+      map.set(str[i], 1);
+    }
+  }
+  const sort = Array.from(map.entries()).sort((a, b) => a[1] - b[1]);
+  const most = sort[sort.length - 1][0];
+  return most;
+}
+
+function main() {
+  const rawData = fs
+    .readFileSync("./input.txt", "utf-8")
+    .split("\n")
+    .map((item) => String(item));
+
+  let data = rawData.map((str) => str.split(""));
+  //console.log(data);
+  let stringMapping = [];
+  for (let j = 0; j < data[0].length; j++) {
+    let colStr = "";
+    for (let i = 0; i < data.length; i++) {
+      colStr += data[i][j];
+    }
+    stringMapping.push(colStr);
+  }
+  let errorCode = "";
+  stringMapping.forEach((item) => {
+    let mostFreq = mostFrequentElement(item);
+    errorCode += mostFreq;
+  });
+  console.log(errorCode);
+}
+
+main();
